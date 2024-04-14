a37e8e6a244375922cefd8a70f1e50aca27f6fbe
D0Agonia <agoniadavid1224@gmail.com>
2024-04-15 00:09:17 +0800

    Add: Index and Class Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ISwhite': '#FEFEFE',
+        'ISshade1': '#8B8B8B',
+        'ISshade2': '#383838',
+        'ISshade3': '#222222',
+        'ISorange': '#F84502',
+        'ISyellow': '#F9C91F',
+        'ISorange0': '#F8450255',
+        'ISyellow0': '#F9C91F55',
+        'IScyan': '#11CBFE',
+        'ISblue': '#4862FF',
+        'IScyan0': '#11CBFE55',
+        'ISblue0': '#4862FF55'
+      }
+    },
   },
   plugins: [],
 }
