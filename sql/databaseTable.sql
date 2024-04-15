b8c823386e7a28fb596c6f1020d1f1ccfd38e084
Kenji-KLD <gabunada.kenji.kld@gmail.com>
2024-04-15 13:06:05 +0800

    updated databaseTable

--- sql/databaseTable.sql
+++ sql/databaseTable.sql
@@ -6,12 +6,17 @@ CREATE TABLE Account(
 	mName VARCHAR(32),
 	lName VARCHAR(32) NOT NULL,
 	email VARCHAR(64) NOT NULL,
+	age INT NOT NULL,
+	gender VARCHAR(6) NOT NULL,
+	address TEXT NOT NULL,
 	PRIMARY KEY(user_ID)
 );
 
 CREATE TABLE Subject(
 	subject_ID VARCHAR(9),
 	subjectName VARCHAR(64) NOT NULL,
+	unitsAmount INT NOT NULL,
+	subjectType VARCHAR(10) NOT NULL,
 	PRIMARY KEY(subject_ID)
 );
 
@@ -42,34 +47,6 @@ CREATE TABLE Faculty(
 	FOREIGN KEY(user_ID) REFERENCES Account(user_ID)
 );
 
-CREATE TABLE Questionnaire(
-	questionnaire_ID BIGINT AUTO_INCREMENT,
-	faculty_ID BIGINT NOT NULL,
-	subject_ID VARCHAR(9) NOT NULL,
-	questionnaireTitle VARCHAR(64) NOT NULL,
-	questionnaireType VARCHAR(4) NOT NULL,
-	questionTotal INT NOT NULL,
-	deadline DATETIME NOT NULL,
-	acadYear VARCHAR(9) NOT NULL,
-	acadTerm VARCHAR(8) NOT NULL,
-	acadSem VARCHAR(15) NOT NULL,
-	PRIMARY KEY(questionnaire_ID),
-	FOREIGN KEY(faculty_ID) REFERENCES Faculty(faculty_ID),
-	FOREIGN KEY(subject_ID) REFERENCES Subject(subject_ID)
-);
-
-CREATE TABLE QuestionBank(
-	question_ID BIGINT AUTO_INCREMENT,
-	questionnaire_ID BIGINT NOT NULL,
-	questionFormat VARCHAR(32) NOT NULL,
-	questionNumber INT NOT NULL,
-	questionText TEXT NOT NULL,
-	questionChoices TEXT,
-	questionAnswer TEXT,
-	PRIMARY KEY(question_ID),
-	FOREIGN KEY(questionnaire_ID) REFERENCES Questionnaire(questionnaire_ID)
-);
-
 CREATE TABLE LoginToken(
 	session_token VARCHAR(32),
 	user_ID BIGINT NOT NULL,
@@ -78,29 +55,83 @@ CREATE TABLE LoginToken(
 	FOREIGN KEY(user_ID) REFERENCES Account(user_ID)
 );
 
+CREATE TABLE QuestionSet(
+	questionSet_ID BIGINT AUTO_INCREMENT,
+	faculty_ID BIGINT NOT NULL,
+	subject_ID VARCHAR(9) NOT NULL,
+	questionSetTitle VARCHAR(64) NOT NULL,
+	questionSetType VARCHAR(4) NOT NULL,
+	questionTotal INT NOT NULL,
+	rubrics TEXT,
+	deadline DATETIME NOT NULL,
+	acadYear VARCHAR(9) NOT NULL,
+	acadTerm VARCHAR(8) NOT NULL,
+	acadSem VARCHAR(15) NOT NULL,
+	PRIMARY KEY(questionSet_ID),
+	FOREIGN KEY(faculty_ID) REFERENCES Faculty(faculty_ID),
+	FOREIGN KEY(subject_ID) REFERENCES Subject(subject_ID)
+);
+
+CREATE TABLE QuestionBank(
+	question_ID BIGINT AUTO_INCREMENT,
+	questionSet_ID BIGINT NOT NULL,
+	questionFormat VARCHAR(32) NOT NULL,
+	questionNumber INT NOT NULL,
+	questionText TEXT NOT NULL,
+	PRIMARY KEY(question_ID),
+	FOREIGN KEY(questionSet_ID) REFERENCES QuestionSet(questionSet_ID)
+);
+
+CREATE TABLE ChoiceBank(
+	choice_ID BIGINT AUTO_INCREMENT,
+	question_ID BIGINT NOT NULL,
+	choiceLabel TEXT NOT NULL,
+	choiceText TEXT NOT NULL,
+	choiceAnswer TEXT,
+	pointsGiven INT NOT NULL,
+	PRIMARY KEY(choice_ID),
+	FOREIGN KEY(question_ID) REFERENCES QuestionBank(question_ID)
+);
+
+CREATE TABLE AnswerStatistic(
+	student_ID VARCHAR(13) NOT NULL,
+	question_ID BIGINT NOT NULL,
+	studentAnswer TEXT NOT NULL,
+	isCorrect BOOLEAN NOT NULL,
+	FOREIGN KEY(student_ID) REFERENCES Student(student_ID),
+	FOREIGN KEY(question_ID) REFERENCES QuestionBank(question_ID)
+);
+
 CREATE TABLE Score(
 	student_ID VARCHAR(13) NOT NULL,
-	questionnaire_ID BIGINT NOT NULL,
+	questionSet_ID BIGINT NOT NULL,
+	passed BOOLEAN NOT NULL,
 	score INT NOT NULL,
 	dateTaken DATETIME NOT NULL,
-	answerStatistics TEXT NOT NULL,
 	FOREIGN KEY(student_ID) REFERENCES Student(student_ID),
-	FOREIGN KEY(questionnaire_ID) REFERENCES Questionnaire(questionnaire_ID)
+	FOREIGN KEY(questionSet_ID) REFERENCES QuestionSet(questionSet_ID)
 );
 
-CREATE TABLE Blacklist(
+CREATE TABLE SetDisallow(
 	student_ID VARCHAR(13) NOT NULL,
-	questionnaire_ID BIGINT NOT NULL,
+	questionSet_ID BIGINT NOT NULL,
 	isDisallowed BOOLEAN NOT NULL,
 	FOREIGN KEY(student_ID) REFERENCES Student(student_ID),
-	FOREIGN KEY(questionnaire_ID) REFERENCES Questionnaire(questionnaire_ID)
+	FOREIGN KEY(questionSet_ID) REFERENCES QuestionSet(questionSet_ID)
 );
 
-CREATE TABLE Classroom(
+CREATE TABLE Class(
 	student_ID VARCHAR(13) UNIQUE NOT NULL,
 	section_ID VARCHAR(8) NOT NULL,
 	FOREIGN KEY(student_ID) REFERENCES Student(student_ID),
 	FOREIGN KEY(section_ID) REFERENCES Section(section_ID)
+);
+
+CREATE TABLE SectionSubjectList(
+	section_ID VARCHAR(8) NOT NULL,
+	subject_ID VARCHAR(9) NOT NULL,
+	FOREIGN KEY(section_ID) REFERENCES Section(section_ID),
+	FOREIGN KEY(subject_ID) REFERENCES Subject(subject_ID)
 );
 
 CREATE TABLE SubjectHandle(
@@ -113,8 +144,6 @@ CREATE TABLE SubjectHandle(
 CREATE TABLE SectionHandle(
 	faculty_ID BIGINT NOT NULL,
 	section_ID VARCHAR(8) NOT NULL,
-	subject_ID VARCHAR(9) NOT NULL,
 	FOREIGN KEY(faculty_ID) REFERENCES Faculty(faculty_ID),
-	FOREIGN KEY(section_ID) REFERENCES Section(section_ID),
-	FOREIGN KEY(subject_ID) REFERENCES Subject(subject_ID)
+	FOREIGN KEY(section_ID) REFERENCES Section(section_ID)
 );
