c8454ab98ae9cc20042a52ebdc92bc9d64a154a0
Kenji-KLD <gabunada.kenji.kld@gmail.com>
2024-04-22 15:34:36 +0800

    Append: Slight Changes

--- sql/queryList.sql
+++ sql/queryList.sql
@@ -48,3 +48,33 @@ INNER JOIN Subject su ON suh.subject_ID = su.subject_ID
 INNER JOIN SectionHandle seh ON suh.subHandle_ID = seh.subHandle_ID
 INNER JOIN Section se ON seh.section_ID = se.section_ID
 WHERE se.section_ID = ? AND su.subject_ID = ?
+
+
+
+SELECT suh.subject_ID, su.subjectName FROM Subject su
+INNER JOIN SubjectHandle suh ON su.subject_ID = suh.subject_ID
+INNER JOIN Faculty f ON suh.faculty_ID = f.faculty_ID
+WHERE f.faculty_ID = 1;
+
+SELECT se.section_ID FROM Section se
+LEFT JOIN SectionHandle seh ON se.section_ID = seh.section_ID
+LEFT JOIN SubjectHandle suh ON seh.subHandle_ID = suh.subHandle_ID
+WHERE suh.subHandle_ID IS NULL;
+
+SELECT se.section_ID FROM Section se
+INNER JOIN SectionHandle seh ON se.section_ID = seh.section_ID
+INNER JOIN SubjectHandle suh ON seh.subHandle_ID = suh.subHandle_ID
+WHERE suh.subHandle_ID = (SELECT subHandle_ID FROM SubjectHandle WHERE faculty_ID = 1)
+
+
+
+SELECT
+    (SELECT COUNT(*) FROM Student s WHERE s.user_ID = ac.user_ID) AS student_count,
+    (SELECT COUNT(*) FROM Admin ad WHERE ad.user_ID = ac.user_ID) AS admin_count,
+    (SELECT COUNT(*) FROM Faculty f WHERE f.user_ID = ac.user_ID) AS faculty_count
+FROM Account ac
+WHERE ac.userName = ?;
+
+SELECT a.* FROM Account a
+INNER JOIN LoginToken l ON a.user_ID = l.user_ID
+WHERE l.session_token = '2gd7ixncim73p2c7mb2hno9ke9ev8k3n';
