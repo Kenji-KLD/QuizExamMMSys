d3e7c257face3319c6a194cdb8b08757d1cafdf7
Kenji-KLD <gabunada.kenji.kld@gmail.com>
2024-04-23 08:51:46 +0800

    Append: Logout connected to DB

--- php/logout_controller.php
+++ php/logout_controller.php
@@ -4,7 +4,9 @@ require_once 'model.php';
 if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest' && $_SERVER["REQUEST_METHOD"] === "POST"){
     $Model = new Model();
     
-    
+    $Model->deleteSessionToken($_COOKIE['session_token']);
+    setcookie('session_token', '', time() - 3600, '/');
+    setcookie('userDetails', '', time() - 3600, '/');
 
     $Model = null; 
     exit();
