896b75dca15340a63c2b796e8d7799ed01bae99e
D0Agonia <agoniadavid1224@gmail.com>
2024-05-14 00:41:15 +0800

    Amend: Enter and Center

--- dist/js/login.js
+++ dist/js/login.js
@@ -1,48 +1,54 @@
-function initiateLogin(){
-    const jsonData = {
-        userName: $('#Username').val(),
-        password: $('#Password').val(),
-        tac_checkbox: $('#TAC_Checkbox').is(':checked') ? true : false
-    };
+function initiateLogin() {
+  const jsonData = {
+    userName: $("#Username").val(),
+    password: $("#Password").val(),
+    tac_checkbox: $("#TAC_Checkbox").is(":checked") ? true : false,
+  };
 
-    $.ajax({
-        url: "/php/login_controller.php",
-        method: "POST",
-        data: {jsonData: JSON.stringify(jsonData)},
-        dataType: 'json',
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        },
-        success: function(response){
-            if(response.processed == true){
-                switch(response.accountType){
-                    case "STUDENT":
-                        window.location.replace('/dist/userView/html/home.html');
-                        break;
-                    case "FACULTY":
-                        window.location.replace('/dist/profView/html/home-page.html');
-                        break;
-                    case "ADMIN":
-                        window.location.replace('/Admin/adminhome.php');
-                        break;
-                    default:
-                        alert(response.accountType);
-                        window.location.replace('/dist/index.html');
-                        break;
-                }
-            }
-            else{
-                alert(response.error_message);
-            }
-        },
-        error: function(error){
-            console.error(error);
+  $.ajax({
+    url: "/php/login_controller.php",
+    method: "POST",
+    data: { jsonData: JSON.stringify(jsonData) },
+    dataType: "json",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    },
+    success: function (response) {
+      if (response.processed == true) {
+        switch (response.accountType) {
+          case "STUDENT":
+            window.location.replace("/dist/userView/html/home.html");
+            break;
+          case "FACULTY":
+            window.location.replace("/dist/profView/html/home-page.html");
+            break;
+          case "ADMIN":
+            window.location.replace("/Admin/adminhome.php");
+            break;
+          default:
+            alert(response.accountType);
+            window.location.replace("/dist/index.html");
+            break;
         }
-    });
+      } else {
+        alert(response.error_message);
+      }
+    },
+    error: function (error) {
+      console.error(error);
+    },
+  });
 }
 
-jQuery(function() {
-    if(localStorage.getItem('tacRead') == 'true'){
-        document.getElementById("TAC_Checkbox").checked = true;
-    }
+document.getElementById("Password").addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    // Enter key code is 13
+    initiateLogin();
+  }
+});
+
+jQuery(function () {
+  if (localStorage.getItem("tacRead") == "true") {
+    document.getElementById("TAC_Checkbox").checked = true;
+  }
 });
