884cc7f8abd8b3029279c0b1486bede279a5151c
Kenji-KLD <gabunada.kenji.kld@gmail.com>
2024-05-17 02:52:44 +0800

    Append: Change pass working

--- dist/js/profile.js
+++ dist/js/profile.js
@@ -1,5 +1,44 @@
 import { getCookie } from '/dist/js/function.js';
 
+window.changePassword = function () {
+    var oldPass = document.getElementById('oldPass').value;
+    var newPass = document.getElementById('newPass').value;
+    var confNewPass = document.getElementById('confNewPass').value;
+
+    if(confirm("Are you sure you want to change your password") != true){
+    }
+    else if(newPass != confNewPass){
+        alert('Passwords do not match');
+    }
+    else{
+        $.ajax({
+            url: "/php/settings_controller.php",
+            method: "POST",
+            data: {
+                oldPass: oldPass,
+                newPass: newPass
+            },
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            },
+            success: function(response){
+                let data = JSON.parse(response);
+
+                if(data.processed == true){
+                    alert("Success");
+                    location.reload();
+                }
+                else{
+                    alert(data.error_message);
+                }
+            },
+            error: function(error){
+                console.error(error);
+            }
+        });
+    }
+}
+
 jQuery(function() {
     let userDetails = JSON.parse(decodeURIComponent(getCookie('userDetails')));
     let fullName = userDetails['lName'] + ', ' + userDetails['fName'];
